4871446cd6d0bd1b04a076e882c27c89667d5bbc
Kagisokgomo <kagisomalobela29@gmail.com>
2026-01-29 02:51:03 -0800

    0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,22 +1,26 @@
-#!/usr/bin/python3
-"""
-0-pascal_triangle
-"""
-
 def pascal_triangle(n):
-    """
-    Returns a list of lists of integers representing
-    Pascal's triangle of n.
-    """
+    """Generate Pascal's triangle with n rows."""
     if n <= 0:
         return []
-
-    triangle = [[1]]  # first row
-
+    
+    triangle = [[1]]
     for i in range(1, n):
-        row = [1] * (i + 1)
-        for j in range(1, i):
-            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        prev = triangle[-1]
+        row = [1] + [prev[j] + prev[j+1] for j in range(len(prev)-1)] + [1]
         triangle.append(row)
-
     return triangle
+
+def print_pyramid(triangle):
+    """Print Pascal's triangle in a nicely centered pyramid format."""
+    if not triangle:
+        print("[]")
+        return
+    max_width = len("   ".join(map(str, triangle[-1])))
+    for row in triangle:
+        row_str = "   ".join(map(str, row))
+        print(row_str.center(max_width))
+
+# Example usage
+n = 5  # change this to generate more rows
+triangle = pascal_triangle(n)
+print_pyramid(triangle)
